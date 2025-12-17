4d539d6406d876ebe8dc50d34ded0e3a10db3ccf
fergie <doigralesfergie@gmail.com>
2025-12-17 04:35:26 +0300

    add project files

---
+++ src/main/java/com/doigrales/fergie/FergieApplication.java
@@ -0,0 +1,13 @@
+package com.doigrales.fergie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FergieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FergieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doigrales/fergie/FergieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doigrales.fergie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FergieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
